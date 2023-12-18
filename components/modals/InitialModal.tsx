fa0a7eaaf4e6017831912521f376f6836c03d5de
samin <samincgs@gmail.com>
2023-12-18 04:27:53 -0500

    Started making the server creation modal

---
+++ components/modals/InitialModal.tsx
@@ -0,0 +1,114 @@
+'use client';
+
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '../ui/input';
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: 'Server name is required.',
+  }),
+  imageUrl: z.string().min(1, {
+    message: 'Server image is required.',
+  }),
+});
+
+const InitialModal = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      imageUrl: '',
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Dialog open={true}>
+      {/* DIALOG */}
+      <DialogContent className='bg-white text-black p-0 overflow-hidden'>
+        <DialogHeader className='pt-8 px-6'>
+          <DialogTitle className='text-center text-2xl font-bold'>
+            Create your server
+          </DialogTitle>
+          <DialogDescription className='text-center text-zinc-500'>
+            Give your server a personality with a name and an image. You can
+            always change it later.
+          </DialogDescription>
+        </DialogHeader>
+        {/* FORM */}
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+            {/* IMAGE */}
+            <div className='flex items-center justify-center text-center '>
+              Image Upload
+            </div>
+            {/* NAME */}
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem className='px-6'>
+                  <FormLabel className='uppercase font-bold text-xs text-zinc-500 dark:text-secondary/70'>
+                    Server name
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isLoading}
+                      placeholder='Enter server name'
+                      className='bg-zinc-300/50 border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-black'
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter className='bg-gray-100 px-6 py-4'>
+              <Button variant='primary' disabled={isLoading}>
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+export default InitialModal;
