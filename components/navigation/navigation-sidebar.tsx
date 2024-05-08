4b274d3dda1a8f691400534ef7ca90943cc7bfc9
samin <samincgs@gmail.com>
2024-05-07 22:48:26 -0400

    created the frontend version of a button where users can add a server (functionality still needed)

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,10 @@ import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { redirect } from 'next/navigation';
 
+import NavigationAction from './navigation-action';
+import { Separator } from '@/components/ui/separator';
+import { ScrollArea } from '../ui/scroll-area';
+
 const NavigationSidebar = async () => {
   const profile = await currentProfile();
 
@@ -21,8 +25,10 @@ const NavigationSidebar = async () => {
 
   return (
     <div className='space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3'>
-      Navigation Sidebar
+      <NavigationAction />
+      <Separator className='h-[2px] w-12 bg-zinc-300 dark:bg-zinc-700 rounded-md mx-auto' />
     </div>
   );
 };
+
 export default NavigationSidebar;
