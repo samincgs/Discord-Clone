afddf22db1100ed2b815b8f12b1f09ea92dea45d
samin <samincgs@gmail.com>
2024-05-09 03:50:31 -0400

    working on channel sidebar

---
+++ app/(main)/servers/[serverId]/layout.tsx
@@ -0,0 +1,45 @@
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
+import { ServerSidebar } from '@/components/server/server-sidebar';
+
+const ServerIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { serverId: string };
+}) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return auth().redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (!server) {
+    return redirect('/');
+  }
+
+  return (
+    <div className='h-full'>
+      <div className='hidden md:flex flex-col h-full w-60 fixed z-20 inset-y-0'>
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+      <main className='h-full md:pl-60'>{children}</main>
+    </div>
+  );
+};
+export default ServerIdLayout;
