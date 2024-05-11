5bd63224e9556afc7f4ebd6a1aa264a997f76266
samin <samincgs@gmail.com>
2024-05-11 14:13:03 -0400

    working on the invite link for others to join the same server

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -3,12 +3,15 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 import { useModal } from '@/hooks/use-modal-store';
+import { Copy, RefreshCw } from 'lucide-react';
 
 export const InviteModal = () => {
   const { isOpen, onClose, type } = useModal();
@@ -20,14 +23,31 @@ export const InviteModal = () => {
       <DialogContent className='bg-white text-black p-0 max-w-xl'>
         <DialogHeader className='px-8 pt-12'>
           <DialogTitle className='text-center text-2xl font-bold'>
-            Create your server
+            Invite Friends
           </DialogTitle>
-          <DialogDescription className='text-center text-zinc-500 font-medium pt-2'>
-            Give your server a unique identity with a creative name and a custom
-            avatar. You can change both anytime you want.
-          </DialogDescription>
         </DialogHeader>
-        Invite Modal!
+        <div className='p-6'>
+          <Label className='uppercase text-xs font-bold text-zinc-300 dark:text-secondary/70'>
+            Server Invite Link
+          </Label>
+          <div className='flex items-center mt-2 gap-x-2'>
+            <Input
+              className='bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0'
+              value='invite-link'
+            />
+            <Button size='icon'>
+              <Copy className='w-4 h-4' />
+            </Button>
+          </div>
+          <Button
+            variant='link'
+            size='sm'
+            className='text-xs text-zinc-500 mt-4'
+          >
+            Generate a new link
+            <RefreshCw className='w-4 h-4 ml-2' />
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
